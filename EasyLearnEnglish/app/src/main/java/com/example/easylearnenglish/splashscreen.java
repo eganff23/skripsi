de9f96b894bcf1fc8d7c4acd4d5bb42599f53b20
eganff <eganff23@gmail.com>
2023-06-11 22:30:54 +0700

    Skripsi

---
+++ EasyLearnEnglish/app/src/main/java/com/example/easylearnenglish/splashscreen.java
@@ -0,0 +1,31 @@
+package com.example.easylearnenglish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class splashscreen extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 3500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splashscreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION);
+    }
+}
